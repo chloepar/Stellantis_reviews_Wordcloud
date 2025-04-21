dde9508379444773f60c128649f34d851bfefa38
Chloe Partrick <38427953+chloepar@users.noreply.github.com>
2025-04-21 18:34:38 -0400

    Add files via upload

---
+++ Stellantis_Reviews_Word_Cloud.py
@@ -0,0 +1,35 @@
+# %%
+import pandas as pd
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+
+
+
+# %%
+pd.set_option('display.max_info_columns', 1000)
+
+# %%
+df = pd.read_excel("REVIEWS of FCA.xlsx", sheet_name="Reviews")
+stop_words = pd.read_excel("REVIEWS of FCA.xlsx", sheet_name="Stop Words")
+
+
+# %%
+# Convert reviews to a single string
+text = ' '.join(df['REVIEWS'].astype(str))
+
+# %%
+# Convert stop words DataFrame to set
+custom_stopwords = set(stop_words['Stop Words'])
+custom_stopwords.update(STOPWORDS)
+
+# %%
+# Generate word cloud
+wordcloud = WordCloud(width=800, height=400, background_color='white', stopwords=custom_stopwords).generate(text)
+
+plt.figure(figsize=(10, 5))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+plt.show()
+
+
+
